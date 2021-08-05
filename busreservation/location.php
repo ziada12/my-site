0bb3b058fe915a109f274f6a8921c1dcb6ea1653
ziada12 <88475809+ziada12@users.noreply.github.com>
2021-08-05 08:32:30 +0200

    first comment

---
+++ busreservation/location.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Welcome to James Buchanan Pub and Restaurant</title>
+<link rel="stylesheet" type="text/css" href="xres/css/style.css" />
+<link rel="icon" type="image/png" href="xres/images/favicon.png" />
+<link type="text/css" href="css/styles.css" rel="stylesheet" media="all" />
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="header">
+    <h1><a href="index.php"><img src="xres/images/logo.png" class="logo" alt="James Buchanan Pub and Restaurant" /></a></h1>
+        <ul id="mainnav">
+			<li><a href="index.php">Home</a></li>
+            
+       
+    	</ul>
+	</div>
+  
+
+</div>
+</body>
+</html>
